ee4aa1b867cc00278c56dff946bfdb6098868043
robsonmendes_1@outlook.com <robsonmendes_1@outlook.com>
2020-03-14 11:48:33 -0300

    inclusao-projet

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false
+};
+
+/*
+  * Para uma depuração mais fácil no modo de desenvolvimento, você pode importar o seguinte arquivo
+  * para ignorar os quadros de pilha de erros relacionados à zona, como `zone.run`,` zoneDelegate.invokeTask`.
+ *
+ *  Essa importação deve ser comentada no modo de produção, pois terá um impacto negativo
+  * no desempenho se um erro for gerado.
+ */
+// import 'zone.js/dist/zone-error';  // Incluído na CLI Angular.
